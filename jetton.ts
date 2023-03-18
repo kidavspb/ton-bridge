6399056f31366ca41773b6129e6c5f33e5c91ae5
kidavspb <kidavspb2002@gmail.com>
2023-03-18 14:32:02 +0300

    Читайем данные с Jetton master contract

--- jetton.ts
+++ jetton.ts
@@ -1,27 +1,45 @@
-import { Address, TonClient, fromNano } from 'ton';
+import { Address, TonClient, fromNano, Dictionary } from 'ton';
+import { API_TOKEN } from './secret';
 
 const wallet = Address.parse('EQCJO7qT3C3eiPTP9eeC7CXERZ0og4SUn8ztyLp_Sg252IGq');
-const jetton = Address.parse('EQB5PkmgRaFh6UFrM8gb5YTEpCtJXa--TFHcJ_n8bmVnuZVv');
+const JettonWalletSmartContractAdress = Address.parse('EQB5PkmgRaFh6UFrM8gb5YTEpCtJXa--TFHcJ_n8bmVnuZVv');
 
 export const client = new TonClient({
     endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+    apiKey: API_TOKEN,
 });
 
-// const data = await client.callGetMethod(jetton, 'get_wallet_data')
-// console.log(data)
-
-(async () => {
-    let { stack } = await client.callGetMethod(
-        jetton, 
+async function main() {
+    var { stack } = await client.callGetMethod(
+        JettonWalletSmartContractAdress, 
         'get_wallet_data'
     );
     let balance = stack.readBigNumber();
     let owner = stack.readAddress();
-    let jet = stack.readAddress();
-    let jetton_wallet_code = stack.readCell();
+    let jetton = stack.readAddress();
+    let jetton_wallet_code1 = stack.readCell();
     
     console.log('balance', fromNano(balance));
     console.log('owner', owner);
-    console.log('jet', jet);
-    // console.log('jetton_wallet_code', jetton_wallet_code);
-})().catch(e => console.error(e));
+    console.log('jetton', jetton);
+    // console.log('jetton_wallet_code', jetton_wallet_code1);
+
+    var { stack } = await client.callGetMethod(
+        jetton, 
+        'get_jetton_data'
+    );
+    let total_supply = stack.readBigNumber();
+    let mintable = stack.readBigNumber();
+    let admin_address = stack.readAddress();
+    var jetton_content = stack.readCell();
+    let jetton_wallet_code2 = stack.readCell();
+    
+    console.log('total_supply', fromNano(total_supply));
+    // console.log('mintable', mintable);
+    console.log('admin_address', admin_address);
+    console.log('jetton_content', jetton_content.toBoc().toString());
+    // console.log('jetton_wallet_code', jetton_wallet_code2);
+}
+
+
+main()
